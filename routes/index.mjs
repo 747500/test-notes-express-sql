d56532be696c0118caf6a8a8feebb05ad5440ed0
Serge R <747500@gmail.com>
2021-04-17 23:07:44 +0300

    separates routes, removes extra comments

---
+++ routes/index.mjs
@@ -0,0 +1,34 @@
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+import * as mw from '../mw/index.mjs'
+
+const api = express.Router()
+
+api.use(bodyParser.urlencoded({ extended: false }))
+api.use(bodyParser.json())
+
+api.post('/register', mw.user.register)
+
+api.get('/me', mw.user.verify, mw.user.info)
+
+api.post('/login', mw.user.login)
+
+const notesRouter = express.Router()
+
+notesRouter.use(bodyParser.text())
+
+notesRouter.get('/list', mw.note.list)
+notesRouter.put('/', mw.note.create)
+notesRouter.get('/:id', mw.note.read)
+notesRouter.post('/:id', mw.note.update)
+notesRouter.delete('/:id', mw.note.remove)
+
+api.use('/notes', mw.user.verify, notesRouter)
+
+api.get('/shared/:id', (req, res) => res.send('/'))
+
+export {
+	api
+}
--- server.mjs
+++ server.mjs
@@ -1,75 +1,23 @@
 
 import './env.mjs'
 
-
-//import sqlite3 from 'sqlite3'
 import express from 'express'
-import bodyParser from 'body-parser'
-//import session from 'express-session'
-//import sqliteStoreFactory from 'express-session-sqlite'
-import ObjectId from 'bson-objectid'
 import morgan from 'morgan'
 import cors from 'cors'
 
-import * as mw from './mw/index.mjs'
+import { api } from './routes/index.mjs'
 
-//const SqliteStore = sqliteStoreFactory.default(session)
 const app = express()
 const port = process.env.HTTP_PORT
 
 app.use(morgan('dev'))
 app.use(cors())
-/*
-app.use(session({
-	resave: false,
-	saveUninitialized: false,
-	secret: process.env.HTTP_SESSION_SECRET,
-	cookie: { maxAge: 60000 },
-    store: new SqliteStore({
-		driver: sqlite3.Database,
-		// path: ':memory:'
-		path: './tmp/sqlite.db',
-		// Session TTL in milliseconds
-		ttl: 60 * 60 * 24 * 7,
-		//prefix: 'sess:',
-		// Default is 5 minutes.
-		cleanupInterval: 300000,
-    })
-}))
-*/
 
-const hello = (req, res) => {
+app.use('/api/v0', api)
+
+app.get('/', (req, res) => {
 	res.send('Hello World!')
-}
-
-app.get('/', hello)
-
-const router = express.Router()
-
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(bodyParser.json())
-
-router.post('/register', mw.user.register)
-
-router.get('/me', mw.user.verify, mw.user.info)
-
-router.post('/login', mw.user.login)
-
-const notesRouter = express.Router()
-
-notesRouter.use(bodyParser.text())
-
-notesRouter.get('/list', mw.note.list)
-notesRouter.put('/', mw.note.create)
-notesRouter.get('/:id', mw.note.read)
-notesRouter.post('/:id', mw.note.update)
-notesRouter.delete('/:id', mw.note.remove)
-
-router.use('/notes', mw.user.verify, notesRouter)
-
-router.get('/shared/:id', hello)
-
-app.use('/api/v0', router)
+})
 
 app.listen(port, () => {
 	console.log(`Example "Notes" app listening at http://localhost:${port}`)
