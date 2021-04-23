e80cb62787cde49b360f7c3b767db10684a29361
Serge R <747500@gmail.com>
2021-04-23 20:34:56 +0300

    removes extra conlog

--- mw/note/list.mjs
+++ mw/note/list.mjs
@@ -3,9 +3,6 @@ import { Note } from '../../model/index.mjs'
 
 
 function list (req, res, next) {
-
-	console.log(req.query)
-
 /*
 	const limit = parseInt(req.query.limit, 10) || 20
 	const offset = parseInt(req.query.offset, 10) || 0
